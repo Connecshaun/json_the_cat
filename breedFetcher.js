9dae0aed5ff33aaec99137b5e74c7d84ee138122
Shaun Robinson <connecshaun@gmail.com>
2021-11-19 04:55:21 +0000

    Update function to pass test cases

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,23 @@
 
-const request = require('request');
 
+const request = require('request');
 
 const fetchBreedDescription = function(breedName, callback) {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     if (error) {
-      callback(error);
+      return callback(error);
     }
 
     let parsed = JSON.parse(body);
     let breed = parsed[0];
 
-    callback(null, breed.description);
+    if (!breed) {
+      return callback(error, "try again");
+    } else {
+      return callback(null, breed.description);
+    }
   });
 };
 
